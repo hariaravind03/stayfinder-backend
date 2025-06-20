9e95583e99252d34151f7f7355d71ad8c39a94b7
hariaravind03 <hariasokan04@gmail.com>
2025-06-20 20:34:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,13 +38,12 @@ const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   maxAge: 86400 // 24 hours
 };
 
 // Middleware
 app.use(cors(corsOptions));
-app.use(express.json());
 app.use(cookieParser());
 
 // Security headers
